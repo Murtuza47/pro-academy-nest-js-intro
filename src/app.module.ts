f90fccd3b6524a9648005c421733f221b3d07489
Ali Murtaza <a.murtuza007@gmail.com>
2025-07-26 18:06:41 +0500

    chore: init typeorm connection with postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -7,7 +8,21 @@ import { TweetModule } from './tweet/tweet.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule, TweetModule, AuthModule],
+  imports: [
+    UserModule,
+    TweetModule,
+    AuthModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      entities: [],
+      synchronize: true,
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgresql',
+      database: 'pro-academy-nestjs-intro',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

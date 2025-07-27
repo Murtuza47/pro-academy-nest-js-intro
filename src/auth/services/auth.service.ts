9ac0fbb0ba4380bf0c6bdf85e635d01eec5e1cc3
Ali Murtaza <a.murtuza007@gmail.com>
2025-07-27 14:37:23 +0500

    chore: clean auth service

--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -1,25 +1,9 @@
-import { forwardRef, Inject, Injectable } from '@nestjs/common';
-
-import { UserService } from '../../user/services/user.service';
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AuthService {
-  constructor(
-    @Inject(forwardRef(() => UserService))
-    private readonly userService: UserService,
-  ) {}
+  constructor() {}
 
   isAuthenticated: boolean = false;
-  login(body: { email: string; password: string }) {
-    const user = this.userService.users.find(
-      (user) => user.email === body.email && user.password === body.password,
-    );
-
-    if (user) {
-      this.isAuthenticated = true;
-      return 'Login successful';
-    }
-
-    return 'Invalid email or password';
-  }
+  login() {}
 }

bd532c63c514fb89e240b95b89242ab8e4767a10
Ali Murtaza <a.murtuza007@gmail.com>
2025-07-27 14:36:12 +0500

    chore: clean tweet controller

--- src/tweet/services/tweet.service.ts
+++ src/tweet/services/tweet.service.ts
@@ -1,18 +1,7 @@
 import { Injectable } from '@nestjs/common';
 
-import { TweetDto } from '../dtos/tweet.dto';
-
 @Injectable()
 export class TweetService {
   constructor() {}
-  tweets: TweetDto[] = [
-    { id: 1, content: 'Hello World', userId: 1, createdAt: new Date() },
-    { id: 2, content: 'NestJS is great!', userId: 1, createdAt: new Date() },
-    { id: 3, content: 'Learning NestJS', userId: 2, createdAt: new Date() },
-    { id: 4, content: 'TypeScript rocks!', userId: 2, createdAt: new Date() },
-  ];
-
-  getAllTweetsByUserId(userId: number): TweetDto[] {
-    return this.tweets.filter((tweet) => tweet.userId === userId);
-  }
+  getAllTweetsByUserId() {}
 }

964964b127e156a58435cb8e4fcae63b61ef5667
Ali Murtaza <a.murtuza007@gmail.com>
2025-07-27 13:00:34 +0500

    chore: add min lenght class validator in password field

--- src/user/dtos/create-user.dto.ts
+++ src/user/dtos/create-user.dto.ts
@@ -4,6 +4,7 @@ import {
   IsNotEmpty,
   IsOptional,
   IsString,
+  MinLength,
 } from 'class-validator';
 
 export class CreateUserDto {
@@ -27,5 +28,6 @@ export class CreateUserDto {
 
   @IsString()
   @IsNotEmpty()
+  @MinLength(8)
   password: string;
 }

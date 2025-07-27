dcdce6277376e3bf7099006e9cb752e43eb70503
Ali Murtaza <a.murtuza007@gmail.com>
2025-07-27 14:37:42 +0500

    chore: clean auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,11 @@
-import { forwardRef, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 
-import { UserModule } from '../user/user.module';
 import { AuthController } from './controller/auth.controller';
 import { AuthService } from './services/auth.service';
 
 @Module({
   providers: [AuthService],
   controllers: [AuthController],
-  imports: [forwardRef(() => UserModule)],
   exports: [AuthService],
 })
 export class AuthModule {}

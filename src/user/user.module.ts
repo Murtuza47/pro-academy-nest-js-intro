db31a2fe3c3497904a0dc69f89761f0aa73b20d6
Ali Murtaza <a.murtuza007@gmail.com>
2025-07-27 14:38:23 +0500

    chore: clean user module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,14 +1,11 @@
-import { forwardRef, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 
-import { AuthModule } from '../auth/auth.module';
-import { TweetModule } from '../tweet/tweet.module';
 import { UserController } from './controllers/user.controller';
 import { UserService } from './services/user.service';
 
 @Module({
   controllers: [UserController],
   providers: [UserService],
-  imports: [TweetModule, forwardRef(() => AuthModule)],
   exports: [UserService],
 })
 export class UserModule {}

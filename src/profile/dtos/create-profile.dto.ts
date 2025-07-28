abf1d614caa57cf93a00ae48fa654c23953e7fb2
Ali Murtaza <a.murtuza007@gmail.com>
2025-07-28 12:20:28 +0500

    chore: add create profile dto

---
+++ src/profile/dtos/create-profile.dto.ts
@@ -0,0 +1,27 @@
+import { IsDate, IsEnum, IsOptional, IsString } from "class-validator";
+
+export class CreateProfileDto {
+  @IsString()
+  @IsOptional()
+  first_name?: string;
+
+  @IsString()
+  @IsOptional()
+  last_name?: string;
+
+  @IsEnum(['male', 'female'])
+  @IsOptional()
+  gender?: 'male' | 'female' | null;
+
+  @IsDate()
+  @IsOptional()
+  date_of_birth?: Date;
+
+  @IsString()
+  @IsOptional()
+  bio?: string;
+
+  @IsString()
+  @IsOptional()
+  profile_image?: string;
+}

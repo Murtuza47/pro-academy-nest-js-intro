c693c6efb47fbdc04152f683045d76c28a72dd34
Ali Murtaza <a.murtuza007@gmail.com>
2025-07-31 01:09:38 +0500

    chore: enableImplicitConversion set to true to allow automatic type conversion in class trasnformer

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,9 @@ async function bootstrap() {
       whitelist: true, // Strip properties not defined in the DTO
       forbidNonWhitelisted: true, // Reject requests with properties not defined in the DTO
       transform: true, // Automatically transform payloads to DTO instances
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
     }),
   );
   await app.listen(process.env.PORT ?? 3000);

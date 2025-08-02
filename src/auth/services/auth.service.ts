ed6cc78361a482ba61316d8e141cadab504a0aca
Ali Murtaza <a.murtuza007@gmail.com>
2025-08-02 20:44:34 +0500

    chore: add hashing compare password in login service in auth module

--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -1,14 +1,21 @@
-import { forwardRef, Inject, Injectable } from '@nestjs/common';
+import {
+  forwardRef,
+  Inject,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 
 import { UserService } from '../../user/services/user.service';
 import { LoginDto } from '../dtos/login.dto';
 import { SignUpDto } from '../dtos/sign-up.dto';
+import { HashingProvider } from '../providers/hashing-provider/hashing-provider';
 
 @Injectable()
 export class AuthService {
   constructor(
     @Inject(forwardRef(() => UserService))
     private readonly userService: UserService,
+    private readonly hashingProvider: HashingProvider,
   ) {}
 
   async signup(signUpDto: SignUpDto) {
@@ -17,7 +24,21 @@ export class AuthService {
 
   async login(loginDto: LoginDto) {
     const user = await this.userService.findUserByEmail(loginDto.email);
+    const hashedPassword = user.password;
 
-    return user;
+    if (
+      !(await this.hashingProvider.comparePassword(
+        loginDto.password,
+        hashedPassword,
+      ))
+    ) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    return {
+      data: user,
+      success: true,
+      message: 'User logged in successfully',
+    };
   }
 }
